e4c17501a544e741b2af7c4ccdc04f50f382d510
Olena Gladun <helen.gladun@gmail.com>
2020-01-28 23:39:02 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,41 @@ import debounce from 'lodash/debounce'
 import SearchBox from './components/SearchBox'
 import {Spinner} from "./components/Spinner";
 
-// utils
-import {robots} from './utils/db/robots'
+// interfaces
+import {IRobot} from "./interfaces/IRobot";
 
 const CardList = React.lazy(() => import('./components/CardList'));
 
 const App = () => {
   const [searchText, changeSearchText] = useState('');
-  const [robotsList, setRobotsList] = useState(robots);
+  const [robotsList, setRobotsList] = useState([]);
 
   const onChangeHandler = (text: string) => {
     changeSearchText(text);
   };
 
   useEffect(() => {
-    if (searchText) {
-      setRobotsList(
-        robots
-          .filter(
-            robot =>
-              robot
-                .name
-                .toLowerCase()
-                .search(searchText.toLowerCase()) > -1
-          )
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setRobotsList(users));
+  }, []);
+
+  const getRobots = () => robotsList
+      .filter(
+        (robot: IRobot) =>
+          robot
+            .name
+            .toLowerCase()
+            .search(searchText.toLowerCase()) > -1
       )
-    } else {
-      setRobotsList(robots)
-    }}, [searchText]);
+  ;
 
   return (
     <Suspense fallback={<div className="full-screen"><Spinner /></div>}>
       <div className="tc">
         <h1 className="main-title f1">RoboFriends</h1>
         <SearchBox onChangeHandler={debounce(onChangeHandler, 500)}/>
-        {robotsList?.length > 0 && <CardList robots={robotsList} />}
+        {robotsList?.length > 0 && <CardList robots={getRobots()} />}
       </div>
     </Suspense>
   )
